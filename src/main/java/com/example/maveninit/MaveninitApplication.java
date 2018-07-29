5e0a03001485aab08ca34939f16ea21b09fd3318
pulua <0ze0ro0@gmail.com>
2018-07-29 13:33:51 +0900

    init

---
+++ src/main/java/com/example/maveninit/MaveninitApplication.java
@@ -0,0 +1,12 @@
+package com.example.maveninit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaveninitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaveninitApplication.class, args);
+	}
+}
